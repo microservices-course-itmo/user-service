588c6b21ec256e1db49b5ae831ae9b3ab0aaa9f6
Kot1que <me@krvd.ru>
2020-09-27 16:55:41 +0300

    Migrations for roles, missing imports

--- src/main/java/com/wine/to/up/user/service/service/impl/RoleService.java
+++ src/main/java/com/wine/to/up/user/service/service/impl/RoleService.java
@@ -3,6 +3,8 @@ package com.wine.to.up.user.service.service.impl;
 import com.wine.to.up.user.service.domain.dto.RoleDto;
 import com.wine.to.up.user.service.domain.entity.Role;
 import com.wine.to.up.user.service.repository.RoleRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
