694349365ecef36ac2a6a308e461bb38fca59367
brovinnick <brovinnick@gmail.com>
2020-10-04 19:44:56 +0300

    fix: json date formatting

--- src/main/java/com/wine/to/up/user/service/domain/dto/UserDto.java
+++ src/main/java/com/wine/to/up/user/service/domain/dto/UserDto.java
@@ -1,7 +1,6 @@
 package com.wine.to.up.user.service.domain.dto;
 
-import com.wine.to.up.user.service.domain.entity.City;
-import com.wine.to.up.user.service.domain.entity.Company;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +15,18 @@ import java.time.LocalDate;
 @Accessors(chain = true)
 public class UserDto implements AbstractDto<Long> {
     private Long id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "UTC")
+    private Instant createDate;
+
     private String sex;
     private String email;
     private String phoneNumber;
     private CityDto city;
     private Boolean isActivated;
-    private Instant createDate;
     private CompanyDto company;
     private RoleDto role;
     private String password;
