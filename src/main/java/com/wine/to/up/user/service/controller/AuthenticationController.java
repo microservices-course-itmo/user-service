e8baadf1be18fc99f16b9bb95441c73fc8a99696
Artem Davletov <Tomas-Cooper@mail.ru>
2020-10-12 11:33:07 +0300

    WIP: Add FirebaseToken parse and init block

--- src/main/java/com/wine/to/up/user/service/controller/AuthenticationController.java
+++ src/main/java/com/wine/to/up/user/service/controller/AuthenticationController.java
@@ -1,13 +1,9 @@
 package com.wine.to.up.user.service.controller;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import com.wine.to.up.user.service.domain.dto.AuthenticationRequestDto;
-import com.wine.to.up.user.service.domain.dto.UserDto;
 import com.wine.to.up.user.service.domain.entity.User;
 import com.wine.to.up.user.service.exception.EntityNotFoundException;
 import com.wine.to.up.user.service.security.jwt.JwtTokenProvider;
@@ -19,9 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,34 +35,30 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto){
-//        FileInputStream serviceAccount =
-//                    new FileInputStream("/Users/artem/IdeaProjects/user-service/serv.json");
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .setDatabaseUrl("https://testfire-1bc2f.firebaseio.com")
-//                    .build();
-//            FirebaseApp.initializeApp(options);
-//
-//            String idToken = requestDto.getFireBaseToken();
-//            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-//            String phoneNumber = (String) decodedToken.getClaims().get("phoneNumber");
+        try {
+            String idToken = requestDto.getFireBaseToken();
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+            String phoneNumber = (String) decodedToken.getClaims().get("phone_number");
 
-        String phoneNumber = requestDto.getFireBaseToken();
+            User user = userService.getByPhoneNumber(phoneNumber);
 
-        User user = userService.getByPhoneNumber(phoneNumber);
+            if(user == null){
+                throw new EntityNotFoundException(userService.getEntityClass().getName(), phoneNumber);
+            }
 
-        if(user == null){
-            throw new EntityNotFoundException(userService.getEntityClass().getName(), phoneNumber);
+            String accessToken = jwtTokenProvider.createToken(phoneNumber, true);
+            String refreshToken = jwtTokenProvider.createToken(phoneNumber, false);
+
+            Map<Object, Object> response = new HashMap<>();
+
+            response.put("accessToken", accessToken);
+            response.put("refreshToken", refreshToken);
+
+            return ResponseEntity.ok(response);
+        } catch (FirebaseAuthException e) {
+            e.printStackTrace();
         }
 
-        String accessToken = jwtTokenProvider.createToken(phoneNumber, true);
-        String refreshToken = jwtTokenProvider.createToken(phoneNumber, false);
-
-        Map<Object, Object> response = new HashMap<>();
-
-        response.put("accessToken", accessToken);
-        response.put("refreshToken", refreshToken);
-
-        return ResponseEntity.ok(response);
+        return null;
     }
 }
