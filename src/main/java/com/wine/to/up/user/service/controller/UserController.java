4585ee7b62a6bd2af1def771cc9e4c37fece857c
brovinnick <brovinnick@gmail.com>
2020-10-19 03:13:12 +0300

    Add endpoint to get user info by ID

--- src/main/java/com/wine/to/up/user/service/controller/UserController.java
+++ src/main/java/com/wine/to/up/user/service/controller/UserController.java
@@ -3,28 +3,42 @@ package com.wine.to.up.user.service.controller;
 import com.wine.to.up.user.service.domain.dto.ListWineUserDto;
 import com.wine.to.up.user.service.domain.dto.UserDto;
 import com.wine.to.up.user.service.domain.dto.UserRegistrationDto;
-import com.wine.to.up.user.service.domain.entity.ListSubscription;
-import com.wine.to.up.user.service.repository.ListSubscriptionRepository;
+import com.wine.to.up.user.service.domain.response.UserResponse;
 import com.wine.to.up.user.service.service.ListSubscriptionService;
 import com.wine.to.up.user.service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.wine.to.up.user.service.security.UserDtoToUserResponseMapper.getUserResponse;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @Slf4j
 public class UserController {
-    public final UserService userService;
-    public final ListSubscriptionService listSubscriptionService;
+    private final UserService userService;
+    private final ListSubscriptionService listSubscriptionService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDto> findUserByID(@PathVariable Long id) {
+    @GetMapping("/{id}/allinfo")
+    public ResponseEntity<UserDto> findUserInfoByID(@PathVariable Long id) {
         UserDto user = userService.getById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findUserByID(@PathVariable Long id) { ;
+        return new ResponseEntity<>(getUserResponse(userService.getById(id)), HttpStatus.OK);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> findCurrentUser(HttpServletRequest httpServletRequest) {
+        return new ResponseEntity<>(getUserResponse(userService.getCurrentUserInfo(httpServletRequest)), HttpStatus.OK);
     }
 
     @PostMapping
--- src/main/java/com/wine/to/up/user/service/service/UserService.java
+++ src/main/java/com/wine/to/up/user/service/service/UserService.java
@@ -3,28 +3,32 @@ package com.wine.to.up.user.service.service;
 import com.wine.to.up.user.service.domain.dto.UserDto;
 import com.wine.to.up.user.service.domain.dto.UserRegistrationDto;
 import com.wine.to.up.user.service.domain.entity.User;
-import com.wine.to.up.user.service.domain.response.UserResponse;
 import com.wine.to.up.user.service.repository.UserRepository;
+import com.wine.to.up.user.service.security.JwtTokenProvider;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 
 @Service
 public class UserService extends AbstractService<Long, UserDto, User, UserRepository> {
     private final RoleService roleService;
+    private final JwtTokenProvider jwtTokenProvider;
     @Value("${user.role.user.id}")
     private Long ROLE_USER_ID;
 
     @Autowired
     public UserService(UserRepository repository,
                        ModelMapper modelMapper,
-                       RoleService roleService) {
+                       RoleService roleService,
+                       JwtTokenProvider jwtTokenProvider) {
         super(repository, modelMapper);
         this.roleService = roleService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
@@ -44,6 +48,11 @@ public class UserService extends AbstractService<Long, UserDto, User, UserReposi
         userDto.setIsActivated(true);
         userDto.setCreateDate(Instant.now());
         return this.create(userDto);
+    }
+
+    public UserDto getCurrentUserInfo(HttpServletRequest httpServletRequest) {
+        return this.getByPhoneNumber(jwtTokenProvider.getPhoneNumber(
+                jwtTokenProvider.resolveToken(httpServletRequest)));
     }
 
     @Transactional(readOnly = true)
