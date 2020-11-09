0230c8f15e51c2327da0eecafd667fea2f508ca1
brovinnick <brovinnick@gmail.com>
2020-11-09 09:15:59 +0300

    Authentication/registration documentation

--- src/main/java/com/wine/to/up/user/service/controller/AuthenticationController.java
+++ src/main/java/com/wine/to/up/user/service/controller/AuthenticationController.java
@@ -14,6 +14,10 @@ import com.wine.to.up.user.service.domain.dto.UserRegistrationDto;
 import com.wine.to.up.user.service.logging.UserServiceNotableEvents;
 import com.wine.to.up.user.service.security.JwtTokenProvider;
 import com.wine.to.up.user.service.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -39,6 +43,12 @@ public class AuthenticationController {
     @InjectEventLogger
     private EventLogger eventLogger;
 
+    @ApiOperation(value = "User registration",
+            notes = "Description: Creates new user and returns authenticationResponse with token pair",
+            response = AuthenticationResponse.class,
+            responseContainer = "ResponseEntity")
+    @ApiResponses(
+            @ApiResponse(code = 418, message = "Cannot verify token"))
     @PostMapping("/registration")
     public ResponseEntity<AuthenticationResponse> registration(@RequestBody RegistrationRequestDto requestDto) {
         String phoneNumber;
@@ -65,6 +75,13 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationResponse);
     }
 
+    @ApiOperation(value = "Login user",
+            notes = "Description: Authenticates and returns authenticationResponse with tokens pair",
+            response = AuthenticationResponse.class,
+            responseContainer = "ResponseEntity")
+    @ApiResponses({
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 418, message = "Cannot verify token")})
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequestDto requestDto) {
         String phoneNumber;
@@ -93,8 +110,17 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationResponse);
     }
 
-    @PostMapping("/refresh")
-    public ResponseEntity<AuthenticationResponse> refresh(@RequestParam String refreshToken) {
+    @ApiOperation(value = "refresh",
+            notes = "Description: Returns new tokens pair",
+            response = AuthenticationResponse.class,
+            responseContainer = "ResponseEntity")
+    @ApiResponses({
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 418, message = "Cannot verify token")})
+    @PostMapping(path = "/refresh")
+    public ResponseEntity<AuthenticationResponse> refresh(
+            @ApiParam(name = "refreshToken", value = "Token for refreshing", required = true)
+            @RequestParam String refreshToken) {
         String tokenType = jwtTokenProvider.getTokenType(refreshToken);
 
         if (!jwtTokenProvider.validateToken(refreshToken) && tokenType.equals("REFRESH_TOKEN")) {
@@ -117,8 +143,15 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationResponse);
     }
 
+    @ApiOperation(value = "validate",
+            notes = "Description: Validates token and returns OK status",
+            responseContainer = "ResponseEntity")
+    @ApiResponses(
+            @ApiResponse(code = 401, message = "Unauthorized"))
     @PostMapping("/validate")
-    public ResponseEntity<Void> validate(@RequestParam String token) {
+    public ResponseEntity<Void> validate(
+            @ApiParam(name = "token", value = "Token for validation", required = true)
+            @RequestParam String token) {
         if (token.equals(TOKEN_STUB) ||
                 jwtTokenProvider.validateToken(token)) {
             return ResponseEntity.ok().build();
