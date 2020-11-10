bf184e6b4f91ad43369b73f734dbd03d464e69fd
PopovAlAn <sashapopov1996@gmail.com>
2020-11-11 01:28:30 +0300

    Добавлены новые методы

--- src/main/java/com/wine/to/up/user/service/repository/UserSubscriptionsRepository.java
+++ src/main/java/com/wine/to/up/user/service/repository/UserSubscriptionsRepository.java
@@ -1,5 +1,7 @@
 package com.wine.to.up.user.service.repository;
 
+import com.wine.to.up.user.service.domain.entity.Item;
+import com.wine.to.up.user.service.domain.entity.User;
 import com.wine.to.up.user.service.domain.entity.UserSubscription;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserSubscriptionsRepository extends CrudRepository<UserSubscription, String> {
     Iterable<UserSubscription> findAllByItemId(String itemId);
+    Iterable<UserSubscription> findAllByUserId(Long userId);
+    void deleteByItemAndUser(Item item, User user);
 }
