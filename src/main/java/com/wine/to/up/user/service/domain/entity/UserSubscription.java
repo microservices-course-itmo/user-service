d6e7c87fee16dc70bada1cc7ac7edec21d8aa425
PopovAlAn <sashapopov1996@gmail.com>
2020-11-11 23:27:59 +0300

    Изменения в UserSubscription

--- src/main/java/com/wine/to/up/user/service/domain/entity/UserSubscription.java
+++ src/main/java/com/wine/to/up/user/service/domain/entity/UserSubscription.java
@@ -2,13 +2,8 @@ package com.wine.to.up.user.service.domain.entity;
 
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.IdClass;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,15 +15,28 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 @Table(name = "list_subscriptions")
-@IdClass(UserSubscription.class)
+@IdClass(UserSubscriptionPk.class)
 public class UserSubscription implements AbstractEntity<String>, Serializable {
     @Id
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private User user;
+    @Column(name = "user_id")
+    protected Long userId;
 
     @Id
-    @JoinColumn(name = "item_id", insertable = false, updatable = false)
+    @Column(name = "item_id")
+    protected String itemId;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    private Item item;
+    @JoinColumn(name = "user_id", insertable = false, updatable = false)
+    protected User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item_id", referencedColumnName = "id", insertable = false, updatable = false)
+    protected Item item;
+
+    public UserSubscription(User user, Item item) {
+        this.user = user;
+        this.userId = user.getId();
+        this.item = item;
+        this.itemId = item.getId();
+    }
 }
