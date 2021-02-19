cf5329423e48e262b63f82f7776dcba1c187a870
brovinnick <brovinnick@gmail.com>
2021-02-19 23:16:49 +0300

    Added endpoint in UserController.updateUserInfo allowing to change user's info

--- src/main/java/com/wine/to/up/user/service/controller/UserController.java
+++ src/main/java/com/wine/to/up/user/service/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.wine.to.up.user.service.controller;
 
+import com.wine.to.up.commonlib.security.AuthenticationProvider;
 import com.wine.to.up.user.service.api.dto.UserResponse;
 import com.wine.to.up.user.service.domain.dto.UserDto;
+import com.wine.to.up.user.service.service.CityService;
 import com.wine.to.up.user.service.service.UserService;
 import io.swagger.annotations.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @Api(value="UserController", description="Operations with users")
 public class UserController {
     public final UserService userService;
+    public final CityService cityService;
     public final ModelMapper modelMapper;
 
     @ApiOperation(value = "Find user by id",
@@ -44,10 +48,23 @@ public class UserController {
 
     @ApiOperation(value = "Get info about logged in user", authorizations = { @Authorization(value="jwtToken") })
     @GetMapping("/me")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<UserResponse> findCurrentUserInfo(HttpServletRequest httpServletRequest) {
         return new ResponseEntity<>(
             modelMapper.map(userService.getCurrentUserInfo(httpServletRequest), UserResponse.class),
             HttpStatus.OK
         );
+    }
+
+    @ApiOperation(value = "Change user's info",
+            notes = "Description: Updates info about logged in user. Parameters cityId and name are optional and only not null fields will affect user's record")
+    @PatchMapping("/me")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Void> updateUserInfo(Long cityId, String name) {
+        UserDto user = userService.getById(AuthenticationProvider.getUser().getId());
+        if (cityId != null) user.setCity(cityService.getById(cityId));
+        if (name != null) user.setName(name);
+        userService.update(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
