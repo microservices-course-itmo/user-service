1523c585dfdcfed654c3b2147437ffd723f4c77c
Kot1que <me@krvd.ru>
2021-02-22 17:20:28 +0300

    small docs fix

--- src/main/java/com/wine/to/up/user/service/controller/UserController.java
+++ src/main/java/com/wine/to/up/user/service/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.wine.to.up.user.service.controller;
 
-import com.wine.to.up.commonlib.security.AuthenticationProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
@@ -10,48 +9,56 @@ import com.wine.to.up.user.service.domain.dto.UpdateUserInfoRequest;
 import com.wine.to.up.user.service.domain.dto.UserDto;
 import com.wine.to.up.user.service.service.CityService;
 import com.wine.to.up.user.service.service.UserService;
-import io.swagger.annotations.*;
-import javax.servlet.http.HttpServletRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @Slf4j
-@Api(value="UserController", description="Operations with users")
+@Api(value = "UserController", description = "Operations with users")
 public class UserController {
     public final UserService userService;
     public final CityService cityService;
     public final ModelMapper modelMapper;
 
     @ApiOperation(value = "Find user by id",
-                    notes = "Description: Returns user and httpStatus OK or error code",
-                    response = UserDto.class,
-                    responseContainer = "ResponseEntity")
+        notes = "Description: Returns user and httpStatus OK or error code",
+        response = UserDto.class,
+        responseContainer = "ResponseEntity")
     @GetMapping(path = "/{id}/full", produces = "application/json")
     public ResponseEntity<UserDto> findUserByID(
-            @ApiParam(name = "id", value = "User's ID", required = true)
-            @PathVariable Long id) {
+        @ApiParam(name = "id", value = "User's ID", required = true)
+        @PathVariable Long id) {
         UserDto user = userService.getById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
     @ApiOperation(value = "User info by ID",
-            notes = "Description: Returns information about user by ID",
-            response = UserResponse.class,
-            responseContainer = "ResponseEntity")
+        notes = "Description: Returns information about user by ID",
+        response = UserResponse.class,
+        responseContainer = "ResponseEntity")
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> findUserInfoByID(@PathVariable Long id) {
         return new ResponseEntity<>(modelMapper.map(userService.getById(id), UserResponse.class), HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Get info about logged in user", authorizations = { @Authorization(value="jwtToken") })
+    @ApiOperation(value = "Get info about logged in user", authorizations = {@Authorization(value = "jwtToken")})
     @GetMapping("/me")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<UserResponse> findCurrentUserInfo() {
@@ -63,9 +70,8 @@ public class UserController {
 
     @ApiOperation(
         value = "Change user's info",
-        notes = "Description: Updates info about logged in user. " +
-                "Parameters cityId and name are optional and only not null fields will affect user's record",
-        authorizations = { @Authorization(value="jwtToken") }
+        notes = "Description: Updates info about logged in user. Only non-null fields will be updated.",
+        authorizations = {@Authorization(value = "jwtToken")}
     )
     @PatchMapping("/me")
     @PreAuthorize("isAuthenticated()")
