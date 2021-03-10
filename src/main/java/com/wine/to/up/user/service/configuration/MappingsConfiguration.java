945258f4d5791bb93558fc3f9e9f1d7404f5ea14
Alexander <sashapopov1996@gmail.com>
2021-03-10 19:54:54 +0300

    Favorites mapping configured

--- src/main/java/com/wine/to/up/user/service/configuration/MappingsConfiguration.java
+++ src/main/java/com/wine/to/up/user/service/configuration/MappingsConfiguration.java
@@ -2,6 +2,8 @@ package com.wine.to.up.user.service.configuration;
 
 import com.wine.to.up.user.service.api.dto.UserResponse;
 import com.wine.to.up.user.service.domain.dto.UserDto;
+import com.wine.to.up.user.service.domain.dto.UserFavoritesDto;
+import com.wine.to.up.user.service.domain.entity.UserFavorites;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
@@ -20,6 +22,7 @@ public class MappingsConfiguration {
         modelMapper.getConfiguration().setAmbiguityIgnored(true)
                                       .setMatchingStrategy(MatchingStrategies.STRICT);
         this.configureUserResponseMapping();
+        this.configureFavoritesMapping();
     }
 
     private void configureUserResponseMapping() {
@@ -28,5 +31,12 @@ public class MappingsConfiguration {
 
         typeMap.addMapping(src -> src.getRole().getName(), UserResponse::setRole);
         typeMap.addMapping(src -> src.getCity().getId(), UserResponse::setCityId);
+    }
+
+    private void configureFavoritesMapping() {
+        TypeMap<UserFavoritesDto, UserFavorites> typeMap =
+                modelMapper.createTypeMap(UserFavoritesDto.class, UserFavorites.class);
+        typeMap.addMapping(src -> src.getItem().getId(), UserFavorites::setItemId);
+        typeMap.addMapping(src -> src.getUser().getId(), UserFavorites::setUserId);
     }
 }
